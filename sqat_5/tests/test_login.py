961f864082d23f2a9c6eb26dbd20af5a8e3e983b
Yershuaq <154915800+Yershuaq@users.noreply.github.com>
2026-01-21 02:13:01 +0500

    Enhance test_login_positive with assertion
    Add assertion to verify user is redirected to Dashboard.

--- sqat_5/tests/test_login.py
+++ sqat_5/tests/test_login.py
@@ -2,10 +2,7 @@ import logging
 from selenium.webdriver.common.by import By
 
 def test_login_positive(driver):
-    """
-    Expected: User is redirected to Dashboard.
-    Actual: Check if 'Dashboard' header is visible.
-    """
+
     logging.info("Шаг 1: Переход на страницу логина")
     driver.get("https://opensource-demo.orangehrmlive.com/")
     
@@ -14,7 +11,7 @@ def test_login_positive(driver):
     driver.find_element(By.NAME, "password").send_keys("admin123")
     driver.find_element(By.TAG_NAME, "button").click()
     
-    # Анализ результата
     header = driver.find_element(By.TAG_NAME, "h6").text
     logging.info(f"Анализ: Ожидаем 'Dashboard', получили '{header}'")
+
     assert header == "Dashboard", f"Ошибка! Ожидалось 'Dashboard', но получили '{header}'"
